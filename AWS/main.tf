25ce452d85051cb5a5d83a7924589f0411bea341
Dmitry Demitov <demitov@gmail.com>
2022-03-15 08:40:14 +0400

    add spaces

--- AWS/main.tf
+++ AWS/main.tf
@@ -19,7 +19,7 @@ terraform {
 
 # провайдер AWS
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   default_tags {
