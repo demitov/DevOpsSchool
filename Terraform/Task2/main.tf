a6488b1769d29717d2d49376bd3f52064d958458
Dmitry Demitov <demitov@gmail.com>
2022-03-16 17:11:30 +0400

    Terraform Task2 get 403 error

---
+++ Terraform/Task2/main.tf
@@ -0,0 +1,65 @@
+#------------------------------------
+# Maintainer: Dmitrii Demitov
+# email: dmitrii_demitov@epam.com
+#------------------------------------
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.1.0"
+    }
+  }
+  required_version = "~>1.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# -------------------------------
+# Data
+data "aws_vpcs" "allow-vpcs" {}
+
+data "aws_subnets" "allow-subnets" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.allow-vpcs.ids
+  }
+}
+
+data "aws_security_groups" "allow-sg" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.allow-vpcs.ids
+  }
+  filter {
+    name   = "group-name"
+    values = ["epam-by-ru"]
+  }
+}
+
+# Get AMI for last Amazon Linux EC2
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
+}
+
+# -------------------------------
+# EC2 instance
+resource "aws_instance" "nginx" {
+  ami                    = data.aws_ami.amazon-linux.id # ami-0d79ebfba811235cd ???
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = data.aws_security_groups.allow-sg.ids
+  subnet_id              = data.aws_subnets.allow-subnets.ids[0]
+  key_name               = "dmitrii_demitov@epam.com"
+
+  tags = {
+    Name  = "Nginx"
+    Owner = "dmitrii_demitov@epam.com"
+  }
+}
